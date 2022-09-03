70a7b92b1f3ac5e9f953b606e8b1f3503ac29ff5
Fang Liu <lfxjtu@gmail.com>
2022-09-04 11:47:17 +1200

    initial commit

---
+++ src/test/java/com/spring/lf/examle/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.lf.examle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
